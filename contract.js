5de2618a13aad96f5b3bcbb8453be89e03af5f26
Sekinatsaliu <x20219253@student.ncirl.ie>
2021-10-30 20:43:54 +0100

    setting up task for day

--- contract.js
+++ contract.js
@@ -1,6 +1,13 @@
 // this is a basic readonly contract interaction file
 const Web3 = require("web3")
 
+// https://ropsten.infura.io/v3/e527f8da4e6b4eeabd327a938f8a3ece
+
+
+//today's lab
+
+//instantiate web3
+
 console.log("hello world?");
 
 
