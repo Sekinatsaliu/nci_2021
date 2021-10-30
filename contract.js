9a81346e56c125c47109a590ead40eda3285b539
Sekinatsaliu <x20219253@student.ncirl.ie>
2021-10-30 22:22:08 +0100

    added ABI

--- contract.js
+++ contract.js
@@ -6,11 +6,12 @@ const Web3 = require("web3")
 
 //instantiate web3
 
-const rpcURL = "https://ropsten.infura.io/v3/";
+const rpcURL = "https://ropsten.infura.io/v3/e527f8da4e6b4eeabd327a938f8a3ece";
 const web3 = new Web3(rpcURL);
 console.log("connected to web3");
 
 // get the ABI (interface) for our contract
+
 // connect to our contract on ropsten
 // run some of the methods in our contract (using javascript)
 
