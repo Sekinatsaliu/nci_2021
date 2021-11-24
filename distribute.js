968508d66d0c64749a96272730c79cb6bf341752
Sekinatsaliu <x20219253@student.ncirl.ie>
2021-11-24 00:11:15 +0000

    tidying distribution

--- distribute.js
+++ distribute.js
@@ -3,15 +3,17 @@
 // in this method:
 // - read the accounts.txt file
 // put the N accounts into an array
-// get the totalsupply for the token owner
+// get the totalsupply remaining for the token owner
 // calculate 5% of that totalSupply
-// loop N times, and execute N transactions transferring the token
+// loop N times, and execute N transactions transferring the token from owner to address in array
 // collect tea and medals
 
-var fs = require("fs")
+let fs = require("fs");
+let BigNumber = require("big-number");
 
 let method = require('./method.js');
 
+// this sets up my .env file
 require('dotenv').config()
 
 infuraToken = process.env.INFURA_TOKEN
@@ -22,6 +24,7 @@ privateKey = Buffer.from(process.env.SUPER_SECRET_PRIVATE_KEY, 'hex')
 const distribute = async() => {
     // read in the file
     let distributionAddresses = fs.readFileSync('./account.txt', 'utf8').split('\n');
+    
     console.log(`distro addresses are: ${ distributionAddresses}`);
 
 }
